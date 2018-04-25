a9f378a0f436b07f447c3674f0fd42d0700c1b94
dancerwinter <jessica.manzano@obf.ateneo.edu>
2018-04-25 12:18:40 +0800

    Added new file

---
+++ Projectile.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.geom.*;
+
+public class Projectile implements DrawingObject {
+	
+	private Rectangle2D.Double r;
+	private double x, y;
+
+	public Projectile() {
+
+	}
+
+	@Override
+	public void draw(Graphics2D g2d) {
+	
+	}
+}
