75f0f498bc016253ce716d0d43e13a210dd0e04c
dancerwinter <jessica.manzano@obf.ateneo.edu>
2018-04-25 15:45:49 +0800

    Changed braces

--- GameServer.java
+++ GameServer.java
@@ -2,43 +2,40 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-public class GameServer
-{
+public class GameServer {
 	private ServerSocket ss;
 	private DataInputStream dataIn;
 	private DataOutputStream dataOut;
 	private int numPlay;
 	
-	public GameServer()
-	{
+	public GameServer() {
 		System.out.println("TheServer has been instantiated");
-		try{
+		try {
 			ss = new ServerSocket(1842);
-		} catch(IOException ex)
-		{
+		} 
+
+		catch(IOException ex) {
 			System.out.println("IOException from server Constructor");	
 		}
 	}
-	public void connectPlayers()
-	{
-		try{
+
+	public void connectPlayers() {
+		 try {
 			System.out.println("Waiting for connection...");
-			while(numPlay < 2)
-			{
+			while(numPlay < 2) {
 				Socket s = ss.accept();
 				numPlay++;
 				System.out.println("Player#" + numPlay + " has connected");
 			}
-			
-		
-		} catch(IOException ex)
-		{
+		}
+
+		catch(IOException ex) {
 			System.out.println("IOException from connectPlayers()");
 		}
 		
 	}
-	public static void main(String args[])
-	{
+
+	public static void main(String args[]) {
 		GameServer gs = new GameServer();
 		gs.connectPlayers();
 	}
