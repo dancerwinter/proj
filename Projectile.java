8e667fee7533f87cf69aac63cf45152ea654c0ce
dancerwinter <jessica.manzano@obf.ateneo.edu>
2018-04-29 15:55:13 +0800

    Projectile

--- Projectile.java
+++ Projectile.java
@@ -5,30 +5,43 @@ public class Projectile implements DrawingObject {
 	
 	private Rectangle2D.Double r;
 	private double x, y;
-	private double speed;
+	
 	private double barrel;
 	private PlayerShip ps;
 
-	public Projectile() {
-		x = 490;
-		y = 365;
+	public Projectile(double a, double b) {
+		x = a;
+		y = b;
 	}
 
 	@Override
 	public void draw(Graphics2D g2d) {
-		r = new Rectangle2D.Double(x, y, 10, 35);
+		r = new Rectangle2D.Double(x, y, 10, 34);
 
 		g2d.setColor(new Color(255, 102, 204));
 		g2d.fill(r);
 	}
 
-	public void fireBullet(double s) {
+	public void fireBullet(double speed, double firingPositionX, double firingPositionY) {
 
-		System.out.println("Bullet fired from Projectile");
-
-		if (y != 0) {
-			y--;
+		x = firingPositionX;
+		
+		if (y != -34) {
+			y -= speed;
 		}
+	}
+
+	public void reloadBullet(double xLocation, double yLocation) {
+		x = xLocation;
+		y = yLocation;
+	}
+
+	public double getPositionX() {
+		return x;
+	}
+
+	public double getPositionY() {
+		return y;
 	}
 
 	// Make a thread in the projectile
