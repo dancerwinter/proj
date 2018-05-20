d45d3689062c3d8e7c8b695b9874ae349e8ca5b0
dancerwinter <jessica.manzano@obf.ateneo.edu>
2018-05-20 14:57:03 +0800

    Applied new fonts

--- Menu.java
+++ Menu.java
@@ -2,6 +2,11 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+// https://docs.oracle.com/javase/tutorial/uiswing/layout/flow.html
+// https://docs.oracle.com/javase/tutorial/uiswing/components/textfield.html
+// https://docs.oracle.com/javase/tutorial/uiswing/layout/box.html
+// https://docs.oracle.com/javase/7/docs/api/java/awt/Font.html
+
 public class Menu extends JFrame{
 
 	private String ip;
@@ -16,7 +21,7 @@ public class Menu extends JFrame{
 	
 	public Menu() {
 		width = 500;
-		height = 175;
+		height = 130;
 
 		ip = "";
 
@@ -28,18 +33,23 @@ public class Menu extends JFrame{
 	public void setUpGUI() {
 		this.setPreferredSize(new Dimension(width, height));
 		this.pack();
-		this.setTitle("Input Box");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
 		this.setLocationRelativeTo(null);
 		this.setResizable(false);
 
-		label = new JLabel("Input the IP Address");
-		address = new JTextField("");
-		enter = new JButton("Okay");
+		container.setLayout(new FlowLayout());
 
-		address.setColumns(12);
+		label = new JLabel("Connect to a Server");
+
+		address = new JTextField("");
+		address.setColumns(18);
+
+		enter = new JButton("Enter");
+
+		Font font = new Font("Impact", Font.PLAIN, 24);
+		label.setFont(font);
+		address.setFont(font);
+		enter.setFont(font);
 
 		container.add(label);
 		container.add(address);
@@ -62,5 +72,6 @@ public class Menu extends JFrame{
 
 	public static void main (String[] args) {
 		Menu menu = new Menu();
+		menu.setUpGUI();
 	}
 }
